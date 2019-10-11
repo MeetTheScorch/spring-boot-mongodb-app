f78ca52bbb7d4e3b16b433452a5390b30d705450
MeetTheScorch <scorch.pl@gmail.com>
2019-10-11 11:13:24 +0200

    splitted database between roles and users

--- src/main/java/webapp/controller/DbPageController.java
+++ src/main/java/webapp/controller/DbPageController.java
@@ -27,71 +27,77 @@ public class DbPageController {
 	@Autowired
 	UserService userService;
 	
-	@GetMapping(value="/db")
-    public String dbPage(Model model) {
+	@GetMapping(value="/db/roles")
+    public String dbRolePage(Model model) {
 		
 		List<Role> rolesList = roleService.findAll();
 		Collections.sort(rolesList, Role.nameComparator);
 		model.addAttribute("roles", rolesList);
 		
+		return "db/roles";
+	}
+	
+	@GetMapping(value="/db/users")
+	public String dbUserPage(Model model) {
+		
 		List<User> usersList = userService.findAll();
 		Collections.sort(usersList, User.nameComparator);
 		model.addAttribute("users", usersList);
 		
-		return "/db/db";
+		return "db/users";
 	}
 	
-	//Role
+	//Roles
 	
-	@PostMapping(path = "/db/role/create")
+	@PostMapping(path = "/db/roles/create")
 	public String createRole(Role role) {
 		@SuppressWarnings("unused")
 		Role newRole = roleService.create(role);
-		return "redirect:/db";
+		return "redirect:/db/roles";
 	}
 	
-	@GetMapping(path = "/db/role/find/{id}")
+	@GetMapping(path = "/db/roles/find/{id}")
 	@ResponseBody
 	public Role findRole(@PathVariable("id") String id) {
 		return roleService.findById(id);
 	}
 	
-	@PostMapping(path = "/db/role/update")
+	@PostMapping(path = "/db/roles/update")
     public String updateRoleWithModal(Role role) {
 		roleService.update(role);
-        return "redirect:/db";
+        return "redirect:/db/roles";
     }
 
-	@GetMapping(path = "/db/role/delete/{id}")
+	@GetMapping(path = "/db/roles/delete/{id}")
 	public String deleteRole(@PathVariable("id") String id, HttpServletRequest request) {
 		roleService.deleteById(id);
-		return "redirect:/db";
+		return "redirect:/db/roles";
 	}
 	
-	//User
+	//Users
 	
-	@PostMapping(path = "/db/user/create")
+	@PostMapping(path = "/db/users/create")
 	public String createUser(User user) {
 		@SuppressWarnings("unused")
 		User newUser = userService.create(user);
-		return "redirect:/db";
+		return "redirect:/db/users";
 	}
 	
-	@GetMapping(path = "/db/user/find/{id}")
+	@GetMapping(path = "/db/users/find/{id}")
 	@ResponseBody
 	public User findUser(@PathVariable("id") String id) {
 		return userService.findById(id);
 	}
 	
-	@PostMapping(path = "/db/user/update")
+	@PostMapping(path = "/db/users/update")
     public String updateUserWithModal(User user) {
 		userService.update(user);
-        return "redirect:/db";
+        return "redirect:/db/users";
     }
 	
-	@GetMapping(path = "/db/user/delete/{id}")
+	@GetMapping(path = "/db/users/delete/{id}")
 	public String deleteUser(@PathVariable("id") String id, HttpServletRequest request) {
 		userService.deleteById(id);
-		return "redirect:/db";
+		return "redirect:/db/users";
 	}
 }
